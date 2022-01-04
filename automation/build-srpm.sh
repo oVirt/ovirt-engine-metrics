49e8d200d43a50cdeccc7383e959272975ee819a
avlitman <64130977+avlitman@users.noreply.github.com>
2022-01-04 14:55:41 +0200

    Add CI build based on GH actions
    Signed-off-by: Aviv Litman <alitman@redhat.com>

---
+++ automation/build-srpm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+
+# git hash of current commit should be passed as the 1st paraameter
+if [ "${GITHUB_SHA}" == "" ]; then
+  GIT_HASH=$(git rev-list HEAD | wc -l)
+else
+  GIT_HASH=$(git rev-parse --short $GITHUB_SHA)
+fi
+
+SUFFIX=".git${GIT_HASH}"
+
+# Directory, where build artifacts will be stored, should be passed as the 1st parameter
+ARTIFACTS_DIR=${1:-exported-artifacts}
+
+# Prepare rpmbuild directory
+[[ -d rpmbuild ]] || mkdir -p rpmbuild
+
+# Prepare source archive
+make dist
+
+# Build source package
+rpmbuild \
+    -D "_topdir rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
+    -ts ovirt-engine-metrics*.tar.gz
